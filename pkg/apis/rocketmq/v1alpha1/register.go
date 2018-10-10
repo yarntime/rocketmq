81e70c211ed1b53f8f435748b196b62b6d7afa9b
yarntime <yarntime@163.com>
2018-10-10 12:31:31 +0800

    change group name

--- pkg/apis/rocketmq/v1alpha1/register.go
+++ pkg/apis/rocketmq/v1alpha1/register.go
@@ -27,7 +27,7 @@ var (
 	AddToScheme   = SchemeBuilder.AddToScheme
 )
 
-const GroupName = "rocketmq.huanwei.io"
+const GroupName = "rocketmq.rivernet.io"
 
 var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
 
