afba7549e9d55b91abcb1d994f898c6ea45f2f9f
yarntime <yarntime@163.com>
2018-10-12 11:55:28 +0800

    bug fix, use the generated config

--- images/scripts/brokerStart.sh
+++ images/scripts/brokerStart.sh
@@ -4,4 +4,4 @@
 #./mqbroker -n rocketmq-namesrv-service.rocketmq-operator:9876
 #./mqbroker -n 192.168.196.49:9876;192.168.122.177:9876
 export JAVA_OPT=" -Duser.home=/opt/store"
-./mqbroker -n $NAMESRV_ADDRESS
+./mqbroker -n $NAMESRV_ADDRESS -c /opt/rocketmq-4.3.0/conf/broker.conf
